14e503c9fc122ea1705a87551f71552ded30fa0f
everett442 <everett442@hotmail.com>
2023-06-05 03:25:30 -0400

    initial commit

---
+++ src/Recipe.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./Recipe.css";
+
+function Recipe({ recipe, deleteRecipe }) {
+  const { name, cuisine, photo, ingredients, preparation } = recipe;
+
+  const handleDelete = () => {
+    deleteRecipe(recipe);
+  };
+
+  return (
+    <tr>
+      <td>{name}</td>
+      <td>{cuisine}</td>
+      <td>
+        <img src={photo} alt={name} />
+      </td>
+      <td className="content_td">
+        <p>{ingredients}</p>
+      </td>
+      <td className="content_td">
+        <p>{preparation}</p>
+      </td>
+      <td>
+        <button name="delete" onClick={handleDelete}>
+          Delete
+        </button>
+      </td>
+    </tr>
+  );
+}
+
+export default Recipe;
